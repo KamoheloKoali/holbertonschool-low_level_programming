4cae77a87f8f2ab7b9e1e2bd8e923b0d0a4aa302
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-20 07:33:28 +0000

    task no.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,8 +4,10 @@
  * Printing to standard output using puts()
  * return 0 to end program if there are no errors
 */
+
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
