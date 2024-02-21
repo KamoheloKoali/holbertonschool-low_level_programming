fce2f214462104af26bcbda48911a321eede1694
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-21 13:08:56 +0000

    task no.9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of single digit numbers
+ * Return: string
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		int putchar(int num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
