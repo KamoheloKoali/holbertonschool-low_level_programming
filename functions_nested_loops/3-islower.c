4548796a0e190c56827675a054017be93cf3e07d
Kamohelo koali <kamohelokoali201@gmail.com>
2024-02-22 14:16:47 +0000

    task no.3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -6,27 +6,8 @@
 
 int _islower(int c)
 {
-        char lower;
-        char upper;
-
-        for (lower = 97;  lower <= 122; lower++)
-        {
-                if (c == lower)
-                {
-                        _putchar('1');
-                        break;
-                }
-        }
-
-        for (upper = 65; upper <= 90; upper++)
-        {
-                if (c == upper)
-                {
-                        _putchar('0');
-                        break;
-                }
-        }
-
-        return (0);
+        if (c >= 97 && c <= 122)
+		return (1);
+ 	return (0);
 }
 
