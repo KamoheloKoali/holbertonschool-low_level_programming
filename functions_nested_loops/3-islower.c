25bbd2448be60f6349b4e0d3c8b0a81a5cb237d2
Kamohelo koali <kamohelokoali201@gmail.com>
2024-02-22 12:20:23 +0000

    task no.3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower -  checks if character is lowercase or uppercase
+ */
+
+int _islower(int c)
+{       
+        char alph = c = '0';
+        char lower;
+        
+        for (lower = 'a';  lower < 'a'; lower++)
+        {       
+                if (alph == lower)
+			return (1);
+		else
+			return (0);
+	}
+}
+
