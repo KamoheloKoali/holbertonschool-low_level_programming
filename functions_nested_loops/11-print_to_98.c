7b77e1e7928ebb5ee024d9e3b4b00283f3898aec
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 13:05:05 +0000

    task no.11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural numbers from any number to 98
+ * @n: variable for parameter
+ * Return: exit point
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		char num = n + '0';
+
+		for (n = n; n <= 98;  n++)
+			printf("%d, ", n);
+		_putchar('\n');
+	}
+
+	else if (n > 98)
+	{
+		for (n = n; n >= 98; n--)
+			printf("%d, ", n);
+		_putchar('\n');
+	}
+}
+
