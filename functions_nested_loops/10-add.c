b8149aa87e0abd975a5f11912bb75ec7746b97a5
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 12:39:44 +0000

    task no.10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @n: variable for first parameter
+ * @n1: variable for 2nd parameter
+ * Return: exit point
+ */
+
+int add(int n, int n1)
+{
+	int result;
+
+	result = n + n1;
+	return (result);
+}
+
