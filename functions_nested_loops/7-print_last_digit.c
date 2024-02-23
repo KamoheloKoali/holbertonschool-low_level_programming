ee9558c894edd544e023a5388ee60249fb0f93c4
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 08:40:37 +0000

    task no.7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,19 @@ int print_last_digit(int n)
 	int last = n % 10;
 	char lastchar = last + '0';
 
-	if (n >= 0)
+	if (n >= 10)
+	{
+		int last = n % 10;
+		char lastchar = last + '0';
+
 		_putchar(lastchar);
+	} else if (n <= -10)
+	{
+		int last = -n % 10;
+		char lastchar = last + '0';
+
+		_putchar(lastchar);
+	}
 	return (0);
 }
 
