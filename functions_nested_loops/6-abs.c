0962e841ec0feb1dd523a23a1d1a5f30c245ebf0
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 07:25:57 +0000

    task no.6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs(int) - tests whether a number is absolute or not
+ * @int: data type for parameter
+ * Return: exit point
+ */
+
+int _abs(int n)
+{
+	int num = (n * 2) + n;
+
+	if (n > 0)
+		_putchar(n);
+	else if (n < 0)
+		_putchar(num);
+	else
+		_putchar(0);
+	return (0);
+}
+
