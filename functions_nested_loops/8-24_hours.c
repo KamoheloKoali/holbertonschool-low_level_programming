6d429b289a2791aab9a5fcd0173a82dad521f89a
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 12:25:10 +0000

    task no.8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,38 +8,22 @@
 
 void jack_bauer(void)
 {
-	char num;
-	char num1;
-	char num2;
-	char num3;
+int hours = 0;
+int min = 0;
 
-	for (num = '0'; num <= '9'; num++)
-	{
-		if ((num3 == '9' && num2 == '0') || (num3 == '9' && num2 == '5' && num1 == '0'))
-			num = '0';
-		
-		for (num1 = '0'; num1 <= '3'; num1++)
-		{
-			if (num3 == '9' && num2 == '0')
-				num1 = '0';
-
-			for (num2 = '0'; num2 <= '5'; num2++)
-			{
-				for (num3 = '0'; num3 <= '9'; num3++)
-				{
-					_putchar(num);
-					_putchar(num1);
-					_putchar(':');
-					_putchar(num2);
-					_putchar(num3);
-					_putchar('\n');
-
-					if (num == '2' && num1 == '3' && num2 == '5' && num3 == '9')
-						return;
-				}
-			}
-		}
-	}
-	
+while (hours < 24)
+{
+while (min < 60)
+{
+_putchar('0' + (hours / 10));
+_putchar('0' + (hours % 10));
+_putchar(':');
+_putchar('0' + (min / 10));
+_putchar('0' + (min % 10));
+_putchar('\n');
+min++;
 }
-
+min = 0;
+hours++;
+}
+}
