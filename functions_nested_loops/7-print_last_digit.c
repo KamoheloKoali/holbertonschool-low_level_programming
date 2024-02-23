50a941a2d1a0f4ae77bca3cf4d692d3be9a08f80
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 08:30:36 +0000

    task no.7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: variable for parameter
+ * Return: exit point
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+	char lastchar = last + '0';
+
+	if (n >= 0)
+		_putchar(lastchar);
+	return (0);
+}
+
