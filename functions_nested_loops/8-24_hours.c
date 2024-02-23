d37f0f8f6bd96bf556961be7f66aed4f50246466
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-23 11:10:54 +0000

    task no.8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print the minute of everyday
+ * @void: no parameter
+ * Return: exit point
+ */
+
+void jack_bauer(void)
+{
+	char num;
+	char num1;
+	char num2;
+	char num3;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if ((num3 == '9' && num2 == '0') || (num3 == '9' && num2 == '5' && num1 == '0'))
+			num = '0';
+		
+		for (num1 = '0'; num1 <= '3'; num1++)
+		{
+			if (num3 == '9' && num2 == '0')
+				num1 = '0';
+
+			for (num2 = '0'; num2 <= '5'; num2++)
+			{
+				for (num3 = '0'; num3 <= '9'; num3++)
+				{
+					if (num < '2' && num1 == '3' && num2 == '5' && num3 == '9')
+					{
+						for (num = '1'; num <= '2'; num++)
+						{
+							for (num1 = '3'; num1 <= '9'; num1++)
+							{
+								for (num2 = '0'; num2 <= '5'; num2++)
+								{
+									for (num3 = '0'; num3 <= '9'; num3++)
+									{
+										_putchar(num);
+										_putchar(num1);
+										_putchar(':');
+										_putchar(num2);
+										_putchar(num3);
+										_putchar('\n');
+
+										if (num == '2' && num1 == '3' && num2 == '5' && num3 == '9')
+										{
+											return;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+}
+
