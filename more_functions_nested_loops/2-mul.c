b251a7bce3e5546722265a7fb7c86918840d2895
Kamohelol Koali <kamohelokoali201@gmail.com>
2024-02-26 08:39:37 +0000

    task no.2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#imclude "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: variable for parameter 1
+ * @b: variable for parameter 2
+ * Returm: exit point
+ */
+
+int mul(int a, int b)
+{
+	result = a * b;
+	return (result);
+}
+
