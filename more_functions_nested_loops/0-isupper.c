ce1dff7c8cb59ad584ca9a1438fb091b534d6a41
Kamohelol Koali <kamohelokoali201@gmail.com>
2024-02-26 07:58:33 +0000

    task no.0, read me file and main.h file

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if an alphabet is a in caps
+ * @c: variable for parameter
+ * Return: exit point
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
