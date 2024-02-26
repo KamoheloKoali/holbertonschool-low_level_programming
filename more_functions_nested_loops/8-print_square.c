0c6b7b58375168387b940e859a4c9e1540abb772
Kamohelol Koali <kamohelokoali201@gmail.com>
2024-02-26 13:05:33 +0000

    task no.8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: variable of paremeter
+ */
+
+void print_square(int size)
+{
+	int num = size;
+	int num1;
+
+	if (size > 0)
+	{
+		for (num; num > 0; num--)
+		{
+			for (num1 = 0; num1 < size; num1++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	} else
+		_putchar('\n');
+}
+
