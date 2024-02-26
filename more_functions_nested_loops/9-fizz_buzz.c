f0eeda7b503c90ebb939f41e50132fd776ee6384
Kamohelol Koali <kamohelokoali201@gmail.com>
2024-02-26 13:59:00 +0000

    task no.9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,10 @@ void fizz_buzz(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) == 0)
-		{
-			if ((num % 3) == 0 && (num % 5) == 0)
+		if ((num % 3) == 0 && (num % 5) == 0)
 				printf("FizzBuzz");
+		else if  ((num 5 3) == 0)
 			printf("Fizz");
-		}
 		else if ((num % 5) == 0)
 			printf("Buzz");
 		else
