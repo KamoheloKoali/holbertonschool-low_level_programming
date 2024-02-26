57c89f431741e376e7918c3a38fd8a8a2c4fc780
Kamohelol Koali <kamohelokoali201@gmail.com>
2024-02-26 08:18:00 +0000

    task no.1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -10,8 +10,8 @@ int _isdigit(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	      return (0);
-	else if (c >= 32)
-		return (0);
+	else if (c > 31)
+		return (2);
 	return (1);
 }
 
