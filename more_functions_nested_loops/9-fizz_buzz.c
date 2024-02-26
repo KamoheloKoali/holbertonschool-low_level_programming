65fd01de48dcf8788d2d486cb618ca7b7143f631
Kamohelol Koali <kamohelokoali201@gmail.com>
2024-02-26 13:43:40 +0000

    task no.9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * fizz_buzz - print numbers from 1 to 100 wwith multiples of 3 or 5 as fizz or buzz and of both 3 and 5 as fizz buzz
+ * Return: exit point
+ */
+
+void fizz_buzz(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0)
+		{
+			if ((num % 3) == 0 && (num % 5) == 0)
+				printf("FizzBuzz ");
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", num);
+	}
+}
+
