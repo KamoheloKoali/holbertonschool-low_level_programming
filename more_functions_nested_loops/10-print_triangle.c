7f33a456cdba3a8d0fd3de619f862cbe25c9445e
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-27 07:55:49 +0000

    task no.10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: variable for parameter
+ */
+
+void print_triangle(int size)
+{
+	int num, margin, num1, to_print;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		num1 = size - 1;
+		for (num = 0; num < size; num++)
+		{
+			margin = 0;
+			while(margin < (num1 - num))
+			{
+				_putchar(' ');
+				margin++;
+			}
+			to_print = size - margin;
+			while (to_print > 0)
+			{
+				_putchar('#');
+				to_print--;
+			}
+			_putchar('\n');
+		}
+}
+
