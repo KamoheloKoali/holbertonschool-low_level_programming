4bcdc1ccd4a30f8ba5f56abf6c646419dfb75e05
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 12:19:24 +0000

    task no.5

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string 
+ * @str: pointer for parameter
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int length1 = length + 1;
+
+	while (str[length])
+	{
+		if (length > 0)
+			_putchar([length1]);
+		else
+			_putchar(str[length]);
+
+	}
+	_putchar('\n')
+}
+
