1f57f839a9019f359129bd39b43df70b0f409b30
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 14:38:32 +0000

    task no.9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: pointer 1 for paremeter 1
+ * @src: pointer 2 for paremeter 2
+ * Return: exit point
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
+
