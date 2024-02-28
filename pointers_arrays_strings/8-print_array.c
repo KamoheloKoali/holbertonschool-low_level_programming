e8a6a8593fe4c9984fee2041e82de62e36284594
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 13:28:58 +0000

    task no.8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print an array
+ * @a: pointer for parameter
+ * @n: variable for parameter
+ */
+
+void print_array(int *a, int n)
+{
+	int length = 0;
+
+	while (a[length])
+	{
+		if (length == n)
+			return;
+		_putchar(a[length]);
+		length++;
+	}
+}
+
