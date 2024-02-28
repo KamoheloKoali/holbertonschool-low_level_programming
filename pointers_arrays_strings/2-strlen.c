2e7ef50872d19e1bd7e18364a412e65ecdc19413
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 08:52:24 +0000

    task no.2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer for parameter
+ * Return: exit point
+ */
+
+int _strlen(char *s)
+{
+	char word[] = *s;
+	int length = sizeof(word)/sizeof(word[0]);
+	return (length);
+}
+
