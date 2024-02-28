03ee1b7b9be1989696a827486e8dca2fda8372f8
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 12:55:00 +0000

    task no.7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half astring
+ * @str: pointer for parameter
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+
+	while (str[length])
+		length++;
+	int length1 = length / 2;
+	length = 0;
+	while (str[length])
+	{
+
+		_putchar(str[length]);
+		if (length == length1)
+			return;
+		length++;
+	}
+}
+
