a1819a76167845ebac354715db9290845208119e
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 10:06:00 +0000

    task no.2

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the screen
+ * @str: pointer for parameter
+ */
+
+void _puts(char *str)
+{
+	int length = 0;
+	char letter;
+
+	while (str[length])
+	{
+		letter = str[length];
+		_putchar(letter);
+		length++;
+	}
+}
+
