18a072a7b1eaa2d828df3234c133bb19965447d7
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-28 08:31:56 +0000

    task no.1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two integers
+ * @a: pointer for parameter one
+ * @b: pointer for parameter two
+ */
+
+void swap_int(int *a, int *b)
+{
+	int num;
+	int num1;
+
+	num = a*;
+	num1 = *b;
+	*a = num1;
+	*b = num;
+}
+
