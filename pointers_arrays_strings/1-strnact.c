6bdaabf824c7d2ea1fcfefe774088d68dcf9f881
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-02-29 12:35:35 +0000

    task no.1

---
+++ pointers_arrays_strings/1-strnact.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat(char *dest, char *src, int n)
+ * @dest: pointer for parameter
+ * @src: pointer for paremeter
+ * @n: integer variable for parameter
+ * Return: exit point
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	 int length = 0, length1 = 0;
+
+	 while (dest[length])
+		 length++;
+	 if (n > 0)
+	 {
+	 	while (src[length1])
+	 	{
+			dest[length] = src[length1];
+		 	if (length == n)
+			 	return;
+		 	length++
+		 	length1++;
+	 	}
+		length++;
+		dest[length] = '\0';
+	 }
+	 return (dest);
+}
+
