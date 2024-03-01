1209f4021e91716d631ba41da125f1d5882cc09d
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 07:50:35 +0000

    task no.3

---
+++ pointers_arrays_strings/strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first pointer for parameter 1
+ * @s2: second pointer for parameter 2
+ * Return: exit point
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int length = 0, length = 0;
+
+	while (s1[length])
+		length++;
+	while (s2[length1])
+		length1++;
+	if (length < length1)
+		return (-1);
+	else if (length > length1)
+		return (1);
+	return (0);
+}
+
