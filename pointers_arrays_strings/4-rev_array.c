be29170de1e599cb37ecdd310a8bbc2bac020bca
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 08:27:55 +0000

    task no.4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: pointer for parameter
+ * @n: variable for parameter
+ */
+
+void reverse_array(int *a, int n)
+{
+	int length = 0, actlength = n;
+
+	while (length < n)
+	{
+		a[length] = a[actlength];
+		actlength--;
+		length++;
+	}
+}
+
