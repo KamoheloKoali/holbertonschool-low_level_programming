a0ec1717c4d3300ec9b69d2ef6082ce093d1cda7
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 11:16:56 +0000

    task no.6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize every first leter of a word
+ * @str: pointer to string
+ * Return: return character
+ */
+
+char *cap_string(char *str)
+{
+	int length = 0, ascii_val;
+
+	while (str[length])
+	{
+		for (ascii_val = 97; ascii_val < 123; ascii_val++)
+		{
+			if (str[length] == str[0] && str[length] == ascii_value)
+				str[length] == ascii_val - 32;
+			for (ascii_val1 = 32; ascii_val1 < 65; ascii_val1++)
+			{
+				if (str[length] == ascii_val1 && str[length + 1] == ascii_val)
+					str[length] == ascii_val - 32;
+			}
+		}
+		length++;
+	}
+	return (str);
+}
+
