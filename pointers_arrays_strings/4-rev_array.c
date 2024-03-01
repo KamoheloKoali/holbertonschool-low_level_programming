9533bcd10c95b1a834aab006f1db80499a314a69
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 09:35:29 +0000

    task no.4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int length = 1, actlength = n - 2, lastel = a[n-1];
+	int length = 0, actlength = n - 1, lastel = a[n-1];
 
 	if (n > 0)
 	{
-		a[lastel] = a[0];
-		a[length] = lastel;
-		while (length < n)
+		while (length < (n - 1))
 		{
 			a[length] = a[actlength];
+			if (firstel != a[actlength])
+				a[actlength] = firstel;
 			actlength--;
 			length++;
 		}
