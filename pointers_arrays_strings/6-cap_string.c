fafbea65c5df9c2c601d856f7e719e69c7720ff1
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 11:18:58 +0000

    task no.6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,18 +8,18 @@
 
 char *cap_string(char *str)
 {
-	int length = 0, ascii_val;
+	int length = 0, ascii_val, ascii_val1;
 
 	while (str[length])
 	{
 		for (ascii_val = 97; ascii_val < 123; ascii_val++)
 		{
 			if (str[length] == str[0] && str[length] == ascii_value)
-				str[length] == ascii_val - 32;
+				str[length] = ascii_val - 32;
 			for (ascii_val1 = 32; ascii_val1 < 65; ascii_val1++)
 			{
 				if (str[length] == ascii_val1 && str[length + 1] == ascii_val)
-					str[length] == ascii_val - 32;
+					str[length] = ascii_val - 32;
 			}
 		}
 		length++;
