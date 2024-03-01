4c3dfc6199718a1e30536a28b680a00b4d4f270c
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 09:57:03 +0000

    task no.4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,7 +9,7 @@
 void reverse_array(int *a, int n)
 {
 	int length = 0, length1, length2, actlength = n - 1;
-	int arr[n];
+	int arr[100];
 
 	for (length1 = 0, length2 = 0; length1 < n; length1++, length2++)
 		arr[length1] = a[length2];
