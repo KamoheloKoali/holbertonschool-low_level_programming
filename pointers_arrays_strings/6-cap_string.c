e3cf70ab83dfc3d7ae8ac4b2282ffbbcab100de7
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 11:57:30 +0000

    task no.6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,11 @@ char *cap_string(char *str)
 			for (ascii_val1 = 32; ascii_val1 < 65; ascii_val1++)
 			{
 				if (str[length] == ascii_val1 && str[length + 1] == ascii_val)
+				{
+					if (str[length] > 48 && str[length] <= 57)
+						continue;
 					str[length + 1] = ascii_val - 32;
+				}
 			}
 		}
 		length++;
