1e1eff6444d0326f0bd4c4cdedbdee8c39c58c75
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-01 12:34:56 +0000

    task no.6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,46 @@
 #include "main.h"
 
+
+
 /**
- * cap_string - capitalize every first leter of a word
- * @str: pointer to string
- * Return: return character
+ * is_sep - Check if a char is a separator.
+ * @s: The char to test
+ *
+ * Return: 1 if separator is found else 0
  */
-
-char *cap_string(char *str)
+int is_sep(char s)
 {
-	int length = 0, ascii_val, ascii_val1, num;
-
-	while (str[length])
-	{
-		for (ascii_val = 97; ascii_val < 123; ascii_val++)
-		{
-			if (str[length] == str[0] && str[length] == ascii_val)
-				str[length] = ascii_val - 32;
-			else if (str[length] == 92)
-				ascii_val += 2;
-			for (ascii_val1 = 32; ascii_val1 < 65; ascii_val1++)
-			{
-				if (str[length] == ascii_val1 && str[length + 1] == ascii_val)
-				{
-					if (str[length] > 48 && str[length] <= 57)
-						continue;
-					str[length + 1] = ascii_val - 32;
-				} else if (str[length] == ascii_val1 && str[length + 1] == ascii_val1)
-				{
-					for (num = 1; num < length; num++)
-					{	
-						_putchar('K');
-						if (str[length + num] == ascii_val)
-							str[length + num] = ascii_val - 32;
-					}
-				}
-			}
-		}
-		length++;
-	}
-	return (str);
+char sep[] = {'\t', '\n', ' ', ',', ';', '!',
+'.', '?', '\"', '(', ')', '{', '}'};
+int i = 0;
+while (sep[i] != '\0')
+{
+if (s == sep[i])
+return (1);
+i++;
+}
+return (0);
+}
+/**
+ * cap_string - This function capitalize all words of a string
+ * @s: string to be processed
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *s)
+{
+int cap, h;
+cap = 1;
+h = 0;
+while (s[h] != '\0')
+{
+if (cap == 1 && (s[h] >= 'a' && s[h] <= 'z'))
+{
+s[h] -= 32;
+cap = 0;
+}
+cap = is_sep(s[h]);
+h++;
+}
+return (s);
 }
 
