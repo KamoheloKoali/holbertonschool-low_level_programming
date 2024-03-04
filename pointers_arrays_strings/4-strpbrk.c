74a614b366f67a048bfe6e6abd771db169bf60da
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-04 14:44:27 +0000

    task no.4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -4,6 +4,7 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: pointer to character
  * @accept: pointer to character
+ * Return: character
  */
 
 char *_strpbrk(char *s, char *accept)
@@ -15,7 +16,7 @@ char *_strpbrk(char *s, char *accept)
 		while (accept[length1])
 		{
 			if (accept[length1] == s[length])
-				return (s + length1);
+				return (a[length1:]);
 			length1++;
 		}
 		length++;
