5bd350f5d3e14c608f5a28023ebe8f43e19ae465
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-04 12:31:06 +0000

    task no.2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	int length = 0, length1 = 0;
-	char str[100];
+	char **str[100];
 
 	while (s[length])
 	{
@@ -18,7 +18,7 @@ char *_strchr(char *s, char c)
 		{
 			while (s[length])
 			{
-				str[length] = s[length];
+				str[length1] = s[length];
 				length++;
 			}
 			return (str);
