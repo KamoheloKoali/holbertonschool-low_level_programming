5de495c737bbce59854c12bb804ac8b32c1407a1
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-04 14:38:22 +0000

    task no.3

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to character
+ * @accept: pointer to character
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int length = 0, length1 = 0;
+
+	while (s[length])
+	{
+		while (accept[length1])
+		{
+			if (accept[length1] == s[length])
+				return (s + length1);
+		}
+	}
+	return (0);
+}
+
