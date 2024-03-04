0c07bb90010942baa3df4d1d08d57df154cd5f58
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-04 10:34:46 +0000

    task no.0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to character
+ * @b: character variable for parameter
+ * @n: integer variable for parameter
+ * Return: character
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int length = 0;
+
+	while (s[length] < n)
+	{
+		s[length] = b;
+		lenght++;
+	}
+	return (s);
+}
+
