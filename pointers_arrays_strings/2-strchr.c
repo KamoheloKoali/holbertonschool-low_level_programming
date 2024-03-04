6f304381498b9a1cb6e4fce5a65d4203f69b0ca1
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-04 11:00:48 +0000

    task no.2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to a character
+ * @c: variable for parameter
+ * Return: character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int length = 0;
+
+	while (s[length])
+	{
+		if (s[length] == c)
+			return (s);
+		length++;
+	}
+	return (NULL);
+}
+
