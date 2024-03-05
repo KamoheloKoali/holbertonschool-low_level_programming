7ee609e599a5c720d0558a9b5884f12f598d2c7c
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-05 08:14:57 +0000

    task no.4

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer to a character
+ * @needle: pointer to a character
+ * Return: character
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int length;
+
+	while(*haystack)
+	{
+		for (length = 0; needle[length] != '\0'; length++)
+			if (needle[length] ==  *haystack)
+				return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
+
