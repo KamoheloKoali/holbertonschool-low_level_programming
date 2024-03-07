a797b98a9dd19be8a9e8bfcb53276497999199f7
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-07 14:06:33 +0000

    task no.6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if an integer is aprime number
+ * @n: variable for parameter
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (prime_number(n, n - 1));
+}
+
+/**
+ * prime_number - help function for is_prime_number
+ * @n: Original number to be tested
+ * @i: Original number to test with
+ *
+ * Return: 1 if a prime else 0
+ */
+
+int prime_number(int n, int i)
+{
+if (i == 1)
+return (1);
+else if (n % i == 0)
+return (0);
+else if (n % i != 0)
+return (prime_number(n, i - 1));
+return (0);
+}
+
--- recursion/main.h
+++ recursion/main.h
@@ -14,3 +14,5 @@ int _pow_recursion(int x, int y);
 
 int _sqrt_recursion(int n);
 
+int is_prime_number(init n);
+
