18cddbfdb7a582ae8c80d6d20aa460bf918d33b0
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-11 08:55:39 +0000

    task no.3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the number of arguments
+ * @argv: an array of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("Error");
+		return (1);
+	}
+	sum = (int) argv[1] * (int) argv[2];
+	printf("%d\n", sum);
+	return (0);
+}
+
