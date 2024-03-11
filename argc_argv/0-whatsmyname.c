b22f60fe249bd5ddb6edd100c0fca119d80e2418
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-11 08:12:20 +0000

    task no.0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <std.h>
+
+/**
+ * main - prints program name
+ * @argc: the number of arguments
+ * @argv: an array of argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
