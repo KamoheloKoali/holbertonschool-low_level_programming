9afd41ad501624f4c6edec7b3d7d24a5710deee1
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-11 09:01:53 +0000

    task no.3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,14 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int sum;
+	int sum, num = atoi(argv[1]), num1 = atoi(argv[2]);
 
 	if (argc == 1)
 	{
 		printf("Error");
 		return (1);
 	}
-	sum =  argv[1] * argv[2];
+	sum = argv[1] * argv[2];
 	printf("%d\n", sum);
 	return (0);
 }
