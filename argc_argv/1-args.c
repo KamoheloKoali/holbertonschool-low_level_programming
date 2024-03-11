839267bd8867688e9695d83194f5d51e67caf81a
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-11 08:22:17 +0000

    task no.1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - prints program name
+ * @argc: the number of arguments
+ * @argv: an array of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv[];
+
+	printf("%d\n", argc);
+	return (0);
+}
+
