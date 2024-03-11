47d74dc3e8a066ed97af27c5ae3cf34fb103aa29
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-11 09:30:49 +0000

    task no.3

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: the number of arguments
+ * @argv: an array of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	} else if (argc == 2)
+		printf("%s\n", argv[1]);
+	else
+		for (i = 1; i < argc; i++)
+		{
+			if ((argv[i] > 32 && argv[i] < 48) || (argv[i] > 57 && argv[i] < 128))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+}
+
