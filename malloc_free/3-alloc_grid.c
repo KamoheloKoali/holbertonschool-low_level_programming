5569e4479b580c48b277d7ceafa956792c5e8b60
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-12 14:08:39 +0000

    task no.3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,26 +10,27 @@
 
 int **alloc_grid(int width, int height)
 {
-        int *data;
-        int **grid;
-	int i;
-
-        if (width <= 0 || height <= 0)
-            return (NULL);
-        grid = malloc(height * sizeof(int *));
-        if (grid == NULL)
-	{
-		free(grid);
-            	return (NULL);
-	}
-        data = malloc(width * height * sizeof(int));
-        if (data == NULL) {
-                free(grid);
-		free(data);
-                return (NULL);
-        }
-        for (i = 0; i < height; i++)
-                grid[i] = data + i * width;
-        return (grid);
+int **arr;
+int r, c, i;
+if (width <= 0 || height <= 0)
+return (NULL);
+arr = (int **)malloc(sizeof(*arr) * height);
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+arr[i] = (int *)malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+while (i--)
+free(arr[i]);
+free(arr);
+return (NULL);
+}
+}
+for (r = 0; r < height; r++)
+for (c = 0; c < width; c++)
+arr[r][c] = 0;
+return (arr);
 }
 
