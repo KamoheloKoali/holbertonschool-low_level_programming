aabf4b8a2e74999bc33b65686424e33171601ffb
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-12 10:41:23 +0000

    task no.1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - allocates memory to a copy of a string
+ * @str: pointer to character
+ * Return: character
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i, length;
+
+	if (str == NULL)
+		return (NULL);
+	for (length = 0; str[length] != '\0'; length++)
+		length++;
+	s = malloc(sizeof(char) * length);
+	for (i = 0; i < length; i++)
+		s[i] = str[i];
+	return (s);
+}
+
