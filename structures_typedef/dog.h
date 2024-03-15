6c72e6790f5420484a398a7e867a600a08b3428d
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-15 12:58:41 +0000

    task no.4

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,6 +19,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
+void _strcopy(char *dst, char *src);
+
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
