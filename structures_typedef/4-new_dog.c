dee9ce8e186a63194dd1e4597c83f52a5ca9e785
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-15 12:48:41 +0000

    task no.4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates new pointer for struct dog
+ * @name: pointer to character
+ * @age: vraiable for parameter
+ * @owner: pointer to character
+ */
+
+dog_t *new_dog(char *name, float age, char *owner) {
+	if (name == NULL || owner == NULL)
+		retur;
+	dog_t *new_dog_ptr = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+		return (NULL);
+	strncpy(new_dog_ptr->name, name, sizeof(new_dog_ptr->name) - 1);
+	new_dog_ptr->name[sizeof(new_dog_ptr->name) - 1] = '\0';
+	strncpy(new_dog_ptr->owner, owner, sizeof(new_dog_ptr->owner) - 1);
+	new_dog_ptr->owner[sizeof(new_dog_ptr->owner) - 1] = '\0';
+	new_dog_ptr->age = age;
+	return (new_dog_ptr);
+}
+
