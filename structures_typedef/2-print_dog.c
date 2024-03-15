6150a3ef18d16f350185745854e943c7b2f7c945
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-15 11:45:33 +0000

    task no.2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
-        	printf("Age: %lf\n", d->age);
+        	printf("Age: %f\n", d->age);
         	printf("Owner: %s\n", d->owner);
 	}
 	else if (d->age < 0)
