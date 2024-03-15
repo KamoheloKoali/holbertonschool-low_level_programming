07865a8f65116c68ab2aa5b89ea859b7486f0f93
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-15 10:32:14 +0000

    task no.0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - stores variables of type float and char
+ * @name: pointer to character
+ * @age: float variable
+ * @owner: pointer to character
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
