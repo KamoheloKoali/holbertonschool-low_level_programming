100a74e192aa070824789b4a39bb2ee7f6ac56ed
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-18 10:47:26 +0000

    task no.1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "functions_pointers.h"
+
+/**
+ * array_iterator - executes a function as a parameter
+ * @array: array to be iterated on
+ * @size: size of array
+ * @action: function that will be used in each element of array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
+
