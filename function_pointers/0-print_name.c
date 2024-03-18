dc80e12cb896f8497ff812e59d7e8bbd9719cfd6
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-18 10:40:00 +0000

    task no.0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name: prints a string.
- * @name: pointer to character.
- * @f: A pointer to a function that prints a string.
+ * print_name - prints a string
+ * @name: pointer to character
+ * @f: A pointer to a function that prints a string
  */
 
 void print_name(char *name, void (*f)(char *))
