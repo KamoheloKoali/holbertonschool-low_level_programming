938887bbf78a5f784ed04c8a6784dfe5a0dbf16c
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-19 10:24:33 +0000

    task no.3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	else if (argv[2] != '+' && argv[2] != '-' && argv[2] != '*' && argv[2] != '/' && argv[2] != '%')
+	else if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
 	{
 		printf("Error\n");
 		exit(99);
