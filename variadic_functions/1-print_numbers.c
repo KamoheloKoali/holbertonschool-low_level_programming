d26712b498d007cba769d5f795843318aac26087
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-20 10:59:19 +0000

    task no.1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -25,9 +25,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("\n");
 	} else
 	{
-		for (i = 0; i <  (n - 1)n; i++)
+		for (i = 0; i <  (n - 1); i++)
 			printf("%d%s", va_arg(args, int), separator);
-		printf("%d\n", va_args(ars, int));
+		printf("%d\n", va_arg(args, int));
 	}
 	va_end(args);
 }
