ed5bd394beab7313a54c18842909d7d12fe9d4ab
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-20 09:55:48 +0000

    task no.0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,11 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum;
+	va_list args;
 
 	if (n == 0)
 		return (0);
-	va_list args;
-	va_start(args, n)
+	va_start(args, n);
 	for (i = 0; i < n; ++i)
 		sum += va_arg(args, int);
 	va_end(args);
