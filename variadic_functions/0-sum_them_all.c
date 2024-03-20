08d5c5a45b9a0e6a58edc0e72ee5c0ebd2575612
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-20 09:51:28 +0000

    task no.0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all of its arguments
+ * @n: number of arguments
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+	va_list args;
+	va_start(args, n)
+	for (i = 0; i < n; ++i)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
+
