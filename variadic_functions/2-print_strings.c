9842175201e6a252e7d2563b71a234f4da4a8754
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-03-20 13:24:30 +0000

    task no.2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,6 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	char *str;
 
 	if (n == 0)
 	{
@@ -22,8 +23,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		va_start(args, n);
 		for (i = 0; i < n; ++i)
 		{
-			if (va_arg(args, char *) != NULL)
-				printf("%s", va_arg(args, char *));
+			str = va_arg(args, char *);
+			if (str != NULL)
+				printf("%s", str);
 			else
 				printf("(nil)");
 			if (separator != NULL)
