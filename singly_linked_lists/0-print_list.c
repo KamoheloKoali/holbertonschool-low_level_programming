8aa21f90464e50155fe6fb214f92ea494b7b1e9f
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-01 12:59:23 +0000

    count number of nodes in linked list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 1;
+	size_t count = 0;
 	const list_t *ptr;
 
 	ptr = h;
@@ -24,5 +24,5 @@ size_t print_list(const list_t *h)
 		ptr = ptr->next;
 		count++;
 	}
-	return (count - 1);
+	return (count);
 }
---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * list_len - counts number of nodes
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count;
+	const list_t *ptr = NULL;
+
+	ptr = h;
+	for (count = 0; ptr != NULL; count++);
+	return (count);
+}
+
