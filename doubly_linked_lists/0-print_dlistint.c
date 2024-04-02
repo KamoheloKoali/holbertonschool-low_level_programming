dc83fae9997fe3eae6f9c9ec33aa9f65f183a653
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-02 13:34:58 +0000

    fix syntax error

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,8 +12,8 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int num = 0;
 
-	if (h == NULL)
-		return (1);
+	if (h == NULL || h == int)
+		return (num);
 	else if (h->next == NULL && h->prev == NULL)
 	{
 		printf("%d\n", h->n);
