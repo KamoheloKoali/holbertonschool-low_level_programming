3fa7469efdeb867a5c75f189818cf6550a462482
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-03 08:21:17 +0000

    Fix handling when trying to add nodes in an empty list... task no.2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,11 +23,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	if (*head == NULL)
 		new->next = NULL;
-	else 
-	{
+	else
 		new->next = *head;
-		(*head) = new;
-	}
 	*head = new;
 	return (new);
 }
