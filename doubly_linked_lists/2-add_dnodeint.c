e66fa01af6373a4d050e442068a5c0aaa9351f1b
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-03 08:09:46 +0000

    Fix syntax errors... task no.2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,20 +15,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return (NULL);
-	if (head == NULL)
 	{
-		new->next = NULL;
-		new->prev = NULL;
-		new->n = n;
-		*head = new;
-		return (*head);
+		free(new);
+		return (NULL);
 	}
-	new->next = *head;
-	new->prev = NULL;
 	new->n = n;
-	(*head)->prev = new;
+	new->prev = NULL;
+	if (*head == NULL)
+		new->next = NULL;
+	else 
+	{
+		new->next = *head;
+		(*head)->new;
+	}
 	*head = new;
-	return (*head);
+	return (new);
 }
 
