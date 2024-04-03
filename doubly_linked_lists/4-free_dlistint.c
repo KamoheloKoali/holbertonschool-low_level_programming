004d3c183ecf940e17af5d1dfd9fb91d4564febf
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-03 09:31:26 +0000

    Add a new node at the end of a doubly linked list... task no.3

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dnodeint - free dlistint_int list
+ * @head: pointer to first node in list
+ * Return: pointer to new node
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlisint_t *current = head;
+
+	if (head == NULL)
+		return;
+	while (current != NULL)
+	{
+		current = current->next;
+		free(head);
+		head = current;
+	}
+}
+
