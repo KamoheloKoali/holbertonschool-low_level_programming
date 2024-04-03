a25e197cb13354b5a6f14ca47f967f733f906266
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-03 10:40:10 +0000

    add all data of dlistint_t list... task no.6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sum_dlistint - add all integers data in the list
+ * @head: pointer to first node in list
+ * Return: pointer to new node
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (current != NULL)
+	{
+		sum += curent->n;
+		current = current->next;
+	}
+	return (sum);
+}
+
