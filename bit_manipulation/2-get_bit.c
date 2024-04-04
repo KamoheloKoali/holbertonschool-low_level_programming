80a53719db9c18441f47cf008f58789e4e61125d
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-04 13:46:04 +0000

    return bit at a given index... task no.2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_bit - get bit at specified position
+ * @n: number
+ * @index: position of wanted bit
+ * Return: bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (0);
+
+	return ((n >> index) & 1);
+}
