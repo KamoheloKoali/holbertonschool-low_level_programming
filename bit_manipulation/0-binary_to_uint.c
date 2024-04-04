06943f134d46aad2ba2418b0fb54e347bf19fdea
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-04 12:34:10 +0000

    fix logical error

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (b[i])
 	{
-		if (b[i] != '1' || b[i] != '0')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
 		ans <<= 1;
 		ans += b[i] - '0';
