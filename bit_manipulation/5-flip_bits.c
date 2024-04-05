2937987fa6e28262f5a6c8b42709530b37c24d3e
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-05 09:15:27 +0000

    fixed syntax error

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * flip_bit - count how many bits need to be flipped in order to convert
+ * flip_bits - count how many bits need to be flipped in order to convert
  * @n: number to be converted to
  * @m: number
  * Return: number of bits needed to be flipped
  */
 
-unsigned int flip_bit(unsigned long int n, unsigned int m)
+unsigned int flip_bits(unsigned long int n, unsigned int m)
 {
 	unsigned int setBits = n ^ m, count = 0;
 
