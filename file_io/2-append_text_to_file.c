cd83af854b0d431b7ba1ddadecb3ecff38a7e3e5
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-06 12:33:35 +0000

    fix logical errors

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,17 +17,21 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	for (count = 0; text_content[count]; count++)
+		;
+	if (write(fd, text_content, count) != count)
 	{
 		close(fd);
 		return (-1);
 	}
-	if (text_content == NULL)
-		close(fd);
-	for (count = 0; text_content[count]; count++)
-		;
-	write(fd, text_content, count);
 	close(fd);
 	return (1);
 }
