86294c6e251d015481e35c793f315aa1be44a56c
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-06 12:01:04 +0000

    added more error handling

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,15 +18,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	test = open(filename, O_RDONLY);
-	if (test > 0)
+	if (test >= 0)
 	{
 		if(ftruncate(test, 0) != 0)
 			return (-1);
 		close(test);
+		return (1);
 	}
 	fd = open(filename, O_CREAT|O_RDWR, 0600);
 	if (fd < 0)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	for (count = 0; text_content[count]; count++)
 		;
 	num = write(fd, text_content, count);
