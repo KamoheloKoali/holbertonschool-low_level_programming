cc2ffc2ea8f3a7bed6479aec2fe6e8efc68eea13
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-06 12:11:32 +0000

    Betty style of coding

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		close(test);
 		return (1);
 	}
-	fd = open(filename, O_CREAT|O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd < 0)
 		return (-1);
 	if (text_content == NULL)
